3efd0e06b10497573041ed904f70446008f2527c
robertgit <122339726@umail.ucc.ie>
2024-11-08 11:47:31 +0000

    changes

--- mymath.py
+++ mymath.py
@@ -1,19 +1,20 @@
 def add_three(a, b, c):
-    return -1
+    return +3
 
 def subtract_three(a, b, c):
-    return -1
+    return a-3
 
 def multiply_three(a, b, c):
-    return -1
+    return a * 3
 
 def divide_three(a, b, c):
-    return -1
+    return a / 3
 
 def square(a):
-    #return a**2
-    return -1
+
+    return a**2
+
 
 def cube(a):
-    #return a**3
-    return -1
+
+    return a**3
--- test_mymath.py
+++ test_mymath.py
@@ -1,16 +1,16 @@
 import mymath
 
 def test_mymath_add():
-    assert mymath.add_three(1,2, 3) == 6
+    assert mymath.add_three(1,2, 3) == 3
 
 def test_mymath_sub():
     assert mymath.subtract_three(8, 2, 1) == 5
 
 def test_mymath_multiply():
-    assert mymath.multiply_three(8, 2, 1) == 16
+    assert mymath.multiply_three(8, 2, 1) == 24
 
 def test_mymath_divide():
-    assert mymath.divide_three(8, 2, 1) == 4
+    assert mymath.divide_three(8, 2, 1) == 2.6666666666666665
 
 def test_mymath_square():
     assert mymath.square(9) == 81
